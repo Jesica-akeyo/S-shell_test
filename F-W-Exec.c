a81142601dd8325068fb1aed5d7db205e3cb60f2
Jesica-akeyo <Jessica.akeyo@gmail.com>
2022-05-10 18:58:05 +0000

    Fork+Wait+Execve

---
+++ F-W-Exec.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+/**
+ * main - fork + wait + execve
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i, status, child_pid;
+	char *argv[] = {"/bin/ls", "-l", "/tmp", NULL};
+
+	for (i = 0; i < 5; i++)
+	{
+		child_pid = fork();
+		if (child_pid == 0)
+		{
+			printf("ppid: %u\n", getppid());
+			if (execve(argv[0], argv, NULL) == -1)
+				perror("Error!\n");
+		}
+		wait(&status);
+	}
+	printf("pid: %u\n", getpid());
+
+	return (0);
+}
