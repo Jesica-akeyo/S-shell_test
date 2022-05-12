077725ef6f560aade3c91bb615689cc671e03728
Jesica-akeyo <Jessica.akeyo@gmail.com>
2022-05-12 10:33:54 +0000

    header

--- main.h
+++ main.h
@@ -4,5 +4,10 @@
 void displaywords(char *WordArray[], int numWords);
 int countSpaces(char theString[]);
 char **splitString(char theString[], int *arraySize);
+char *_getline(void);
+char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, char *src);
+int _strcmp(char *s1, char *s2);
+char *_strdup(char *str);
 
 #endif
